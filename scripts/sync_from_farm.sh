9ca125737d9214dd47d05af13b8a4b239c40afb5
Mike <mikeryanmay@gmail.com>
2024-10-01 13:21:20 -0700

    redoing high mu chromosse analyses

--- scripts/sync_from_farm.sh
+++ scripts/sync_from_farm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # synchronize all
-rsync -azP --exclude=".*/" mrmay@farm.cse.ucdavis.edu:chromohisse_analyze_sims/output/ ~/repos/chromohisse_analyze_sims/output/
+# rsync -azP --exclude=".*/" mrmay@farm.cse.ucdavis.edu:chromohisse_analyze_sims/output/ ~/repos/chromohisse_analyze_sims/output/
+rsync -azP --exclude=".*/" mrmay@farm.cse.ucdavis.edu:chromohisse_analyze_sims/output_low_mu/ ~/repos/chromohisse_analyze_sims/output_low_mu/
